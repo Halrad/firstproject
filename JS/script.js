831bf180cdb9ec4fd5e99271c4cde5625a5577a1
Radik <didika@bk.ru>
2020-10-25 21:36:22 +0300

    First commit

---
+++ JS/script.js
@@ -0,0 +1,103 @@
+// Урок 5
+
+
+// let number = 4.6;
+// const myName = 'Radik';
+// const bool = true; //false
+// let und;
+
+// console.log(number / 0);
+// console.log(myName * 9);
+// console.log(something);
+// console.log(und);
+
+
+
+// Урок 6
+
+// const obj = {
+//     name: 'Radik',
+//     age: '25',
+//     isMarried: false
+// }
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = [ 'plum.png', 'orange.jpg', 'apple.bmp', 6, {}, [] ];
+// console.log(arr[2]);
+
+
+// Урок 7
+// "use strict";
+
+// alert('Hello');
+// let result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt ('Is your age below 18?', '18');
+// const answer = +prompt ('Is your age below 18?', '18');
+// console.log(typeof(answer));
+
+// let answers = [];
+
+// answers[0] = prompt('Whats your name?', '');
+// answers[1] = prompt('Whats your surname?', '');
+// answers[2] = prompt('Whats your age?', '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// Урок 8
+
+// "use strict";
+
+// let category = 'Toys';
+
+// console.log('https://someurl.com/' + category);
+// console.log(`https://someurl.com/${category}/5`); 
+
+// const user = 'Ivan';
+
+// alert(`Hi, ${user}`);
+
+// Урок 9
+
+// "use strict";
+
+// console.log('name' + ' surname');
+// console.log(5 + +"5");
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+// console.log(5==2);
+// console.log(5 === '5');
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked && isClose); // Оба выражения должны быть true - будет  true (все остальное false)
+// console.log(isChecked || isClose); //Если одно из значений true - вернет true
+// console.log(isChecked || !isClose); //! - оператор отрицания. Меняет значение на противоположное
+
+// console.log( 2 + 2 * 2 !=='6')
+
+// Урок 10
+
+// Git init
+// Git status
+// Git add -A
+// Git add *.css
+
